be5af8613e30a3fb224c688b378636220cea8aa5
ol0kat <136162631+ol0kat@users.noreply.github.com>
2023-06-11 14:58:10 +0700

    Update file.js

--- file.js
+++ file.js
@@ -1,61 +1,39 @@
-  // Get the form element
-  var form = document.getElementById("contact-form");
+// Get the form element
+var contactForm = document.getElementById("contact-form");
 
-  // Add a submit event listener to the form
-  form.addEventListener("submit", function(event) {
-    event.preventDefault(); // Prevent the form from submitting
+// Add a submit event listener to the form
+contactForm.addEventListener("submit", function(event) {
+  event.preventDefault(); // Prevent the form from submitting normally
 
-    // Validate the form
-    if (validateForm()) {
-      // If the form is valid, send the message
-      sendMessage();
-    } else {
-      // If the form is not valid, display an error message
-      displayErrorMessage();
-    }
-  });
-
-  // Function to validate the form
-  function validateForm() {
-    // Get the email and message fields
-    var emailField = document.getElementById("email");
-    var messageField = document.getElementById("message");
-
-    // Check if the email and message fields are empty
-    if (emailField.value === "" || messageField.value === "") {
-      return false; // Form is not valid
-    }
-
-    return true; // Form is valid
-  }
-
-  // Function to send the message
-function sendMessage() {
   // Get the email and message fields
   var emailField = document.getElementById("email");
   var messageField = document.getElementById("message");
 
-  // Create an object with the form data
-  var formData = {
-    email: emailField.value,
-    message: messageField.value
-  };
+  // Create a FormData object with the form data
+  var formData = new FormData();
+  formData.append("email", emailField.value);
+  formData.append("message", messageField.value);
 
-  // Send the message using Formspree
-  $.ajax({
-    type: "POST",
-    url: "https://formspree.io/p__tison@outlook.com",
-    data: formData,
-    dataType: "json",
-    success: function(response) {
+  // Send the form data to Formspree using fetch
+  fetch(contactForm.action, {
+    method: "POST",
+    body: formData,
+    headers: {
+      Accept: "application/json"
+    }
+  })
+  .then(function(response) {
+    if (response.ok) {
       // Display a success message
       var successMessage = document.querySelector(".contact_success");
       successMessage.style.display = "block";
-      form.reset(); // Reset the form fields
-    },
-    error: function(error) {
+      contactForm.reset(); // Reset the form fields
+    } else {
       // Display an error message
-      console.error("Error sending message:", error);
+      console.error("Error sending message:", response.statusText);
     }
+  })
+  .catch(function(error) {
+    console.error("Error sending message:", error);
   });
-}
+});
